823e1e567146b3c47435976b9bbefa3b411245c1
Matine temory <temorymatine@gmail.com>
2020-05-27 00:29:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,14 +3,13 @@
 
 def key_for_min_value(name_hash)
   
-  
+  answer = nil
   min_val = nil
   
 name_hash.each{|key,value| 
-min_val = value if min_val == nil || min_val > value
+min_val = value if min_val == nil || min_val > value then
 
-}
-answer = nil
+
  name_hash.each{|k,v| puts k if v == min_val  }
  
 
